ddc1af73e0382634dd0d689ca78b11ca46129867
D1noDev <me@dinodev.org>
2025-03-11 22:09:15 +0100

    testing packages + added node.js installer

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -12,17 +12,40 @@ red='\e[0;31m'
 yel='\e[1;33m'
 
 echo -e "${grn}Welcome to D1noD3v's bootstrap script!${wht}"
-echo "${grn}We will start off with updating & upgrading your package list.${wht}"
+echo -e "${grn}We will start off with updating & upgrading your package list.${wht}"
 
-apt-get update -y
-apt-get upgrade -y
+#apt-get update -y
+#apt-get upgrade -y
 echo "Updating done!"
 
-echo "${grn}Packages to install:${wht}"
+echo -e "${grn}Packages to install:${wht}"
 cat packages.txt
-read -rp "Does this look correct?> (Y/n)" pack_choice
-if [[ pack_choice == "" || pack_choice == "y" ]]; then
+read -rp "Does this look correct?(Y/n)> " pack_choice 
+if [[ $pack_choice == "y" || pack_choice == "" ]]; then
     # Installing packages
     echo "Installing packages from packages.txt..."
     xargs -a packages.txt apt-get install
+else
+    echo -e "${grn}Skipping packages install..."
+fi
+
+read -rp "Would you like to install node.js? (y/N)> " node_choice
+
+if [[ $node_choice == "y" ]]; then
+    # Download and install nvm, taken from node.js installation website
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+    # in lieu of restarting the shell
+    \. "dinodev/.nvm/nvm.sh" # replace with your own home directory
+
+    # Download and install Node.js:
+    nvm install 23
+
+    # Verify the Node.js version:
+    echo -e "${grn}Current Node.js version: ${wht}" && node -v # Should print "v23.9.0".
+    echo -e "${grn}Current node version manager: ${wht}" && nvm current # Should print "v23.9.0".
+
+    # Verify npm version:
+    echo -e "${grn}Current NPM version: ${wht}" && npm -v # Should print "10.9.2".
+
 fi
