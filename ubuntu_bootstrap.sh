ed37e572e36aa4a841e6c24524d42f3f24659fa2
D1noDev <me@dinodev.org>
2025-03-11 19:41:04 +0100

    changed gitconfig

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -5,10 +5,24 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-echo "Welcome to D1noD3v's bootstrap script!"
-echo "We will start off with updating & upgrading your package list."
+grn='\e[0;32m'
+blu='\e[0;34m'
+wht='\e[0;37m'
+red='\e[0;31m'
+yel='\e[1;33m'
+
+echo -e "${grn}Welcome to D1noD3v's bootstrap script!${wht}"
+echo "${grn}We will start off with updating & upgrading your package list.${wht}"
 
 apt-get update -y
 apt-get upgrade -y
+echo "Updating done!"
 
-apt-get install 
+echo "${grn}Packages to install:${wht}"
+cat packages.txt
+read -rp "Does this look correct?> (Y/n)" pack_choice
+if [[ pack_choice == "" || pack_choice == "y" ]]; then
+    # Installing packages
+    echo "Installing packages from packages.txt..."
+    xargs -a packages.txt apt-get install
+fi
